4144d8b5af556f569df57041817c1cd23f99ca31
Niaj Ahammed Shuvo <shuvoniaj@gmail.com>
2026-09-18 21:40:45 +0600

    d

--- src/app.js
+++ src/app.js
@@ -1,60 +1,42 @@
 const API_KEY = "da99b8a46a743f9cb89fc6527d422c87";
-const BASE_URL = `https://api.themoviedb.org/3/`;
-const IMAGE_BASE_URL = `https://image.tmdb.org/t/p/w500`;
+const BASE_URL = "https://api.themoviedb.org/3/";
 
-async function trendingMovies(event) {
-  if (event) event.preventDefault();
 
-  // document.getElementById("heroBanner").style.display = "block";
-  document.getElementById("seeMoreBtn").style.display = "inline-block";
-  document.getElementById("sectionTitle").innerText = "Trending Movies";
+// for theme 
+function toggleTheme () {
+  const htmlElement = document.documentElement;
+  const themeBtn = document.getElementById("themeBtn");
 
-  document.getElementById("homeLink").className = "block text-red-500 font-semibold";
-  document.getElementById("trendingLink").className = "block text-gray-500";
-
-  const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}`;
-
-  await fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayTrendingMovies(data.results.slice(0, 4)));
+  if (htmlElement.classList.contains("dark")) {
+    htmlElement.classList.remove("dark");
+    localStorage.setItem("userTheme", "light");
+    if (themeBtn) {
+      themeBtn.innerText = "🌙";
+    }
+  } else {
+    htmlElement.classList.add("dark");
+    localStorage.setItem("userTheme", "dark");
+    if (themeBtn) {
+      themeBtn.innerText = "☀️";
+    }
+  }
 }
 
-async function allTrendingMovies(event) {
-  if (event) event.preventDefault();
+// run after the HTML DOM is fully loaded
+document.addEventListener("DOMContentLoaded", () => {
+  const savedTheme = localStorage.getItem("userTheme");
+  const htmlElement = document.documentElement;
+  const themeBtn = document.getElementById("themeBtn");
 
-  // document.getElementById("heroBanner").style.display = "none";
-  document.getElementById("seeMoreBtn").style.display = "none";
-  document.getElementById("sectionTitle").innerText = "All Trending Movies";
-
-  document.getElementById("homeLink").className = "block text-gray-500";
-  document.getElementById("trendingLink").className = "block text-red-500 font-semibold";
-
-  const url = `${BASE_URL}trending/movie/week?api_key=${API_KEY}`;
-
-  await fetch(url)
-    .then((res) => res.json())
-    .then((data) => displayTrendingMovies(data.results));
-}
-
-function displayTrendingMovies(movies) {
-  let trendingMovieHTML = "";
-
-  movies.forEach((movie) => {
-    const posterUrl = movie.poster_path ? `${IMAGE_BASE_URL}${movie.poster_path}` : 'https://via.placeholder.com/500x750?text=No+Image';
-
-    trendingMovieHTML += `
-      <a href="">
-        <img src="${posterUrl}" alt="">
-        <h2 class="text-2xl font-bold text-gray-800 my-4">${movie.original_title}</h2>
-        <p class="text-md text-gray-600">${movie.overview ? movie.overview.slice(0, 50) : ''}...</p>
-      </a>
-    `;
-  });
-
-  document.getElementById("movieContainer").innerHTML = trendingMovieHTML;
-}
-
-trendingMovies();
-
-
-
+  if (savedTheme === "dark") {
+    htmlElement.classList.add("dark");
+    if (themeBtn) {
+      themeBtn.innerText = "☀️";
+    }
+  } else {
+    htmlElement.classList.remove("dark");
+    if (themeBtn) {
+      themeBtn.innerText = "🌙"
+    }
+  }
+});
