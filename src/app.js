43f10e6b34ef4910a16fbae4c3c74ec8ad281777
Niaj Ahammed Shuvo <shuvoniaj@gmail.com>
2026-09-19 20:23:57 +0600

    d

--- src/app.js
+++ src/app.js
@@ -1,42 +1,46 @@
 const API_KEY = "da99b8a46a743f9cb89fc6527d422c87";
 const BASE_URL = "https://api.themoviedb.org/3/";
 
+const htmlElement = document.documentElement;
+const themeBtn = document.getElementById("themeBtn");
+
 
 // for theme 
 function toggleTheme () {
-  const htmlElement = document.documentElement;
-  const themeBtn = document.getElementById("themeBtn");
+  
+
+  htmlElement.classList.toggle("dark");
 
   if (htmlElement.classList.contains("dark")) {
-    htmlElement.classList.remove("dark");
-    localStorage.setItem("userTheme", "light");
-    if (themeBtn) {
-      themeBtn.innerText = "🌙";
-    }
+    themeBtn.textContent = "☀️";
+    localStorage.setItem("theme", "dark");
   } else {
-    htmlElement.classList.add("dark");
-    localStorage.setItem("userTheme", "dark");
-    if (themeBtn) {
-      themeBtn.innerText = "☀️";
-    }
+    themeBtn.textContent = "🌙";
+    localStorage.setItem("theme", "light");
   }
 }
 
-// run after the HTML DOM is fully loaded
-document.addEventListener("DOMContentLoaded", () => {
-  const savedTheme = localStorage.getItem("userTheme");
-  const htmlElement = document.documentElement;
-  const themeBtn = document.getElementById("themeBtn");
+if (htmlElement.classList.contains("dark")) {
+  themeBtn.textContent = "☀️";
+} else {
+  themeBtn.textContent = "🌙";
+}
 
-  if (savedTheme === "dark") {
-    htmlElement.classList.add("dark");
-    if (themeBtn) {
-      themeBtn.innerText = "☀️";
-    }
-  } else {
-    htmlElement.classList.remove("dark");
-    if (themeBtn) {
-      themeBtn.innerText = "🌙"
-    }
-  }
-});
+// run after the HTML DOM is fully loaded
+// document.addEventListener("DOMContentLoaded", () => {
+//   const savedTheme = localStorage.getItem("userTheme");
+//   const htmlElement = document.documentElement;
+//   const themeBtn = document.getElementById("themeBtn");
+
+//   if (savedTheme === "dark") {
+//     htmlElement.classList.add("dark");
+//     if (themeBtn) {
+//       themeBtn.innerText = "☀️";
+//     }
+//   } else {
+//     htmlElement.classList.remove("dark");
+//     if (themeBtn) {
+//       themeBtn.innerText = "🌙"
+//     }
+//   }
+// });
